b1803f8ed50e4db270c6e73283732b92b5b1d640
Thanhy Mather <tmather@omnissa.com>
2026-01-31 12:02:35 -0800

    Native AOT test

---
+++ NativeAOTCompatibility.Test/NerdbankMessagePack.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Nerdbank.Streams;
+using PolyType;
+using StreamJsonRpc;
+
+namespace NativeAOTCompatibility.Test;
+
+internal static partial class NerdbankMessagePack
+{
+    internal static async Task RunAsync()
+    {
+        (Stream clientPipe, Stream serverPipe) = FullDuplexStream.CreatePair();
+        JsonRpc serverRpc = new JsonRpc(new LengthHeaderMessageHandler(serverPipe, serverPipe, CreateFormatter()));
+        JsonRpc clientRpc = new JsonRpc(new LengthHeaderMessageHandler(clientPipe, clientPipe, CreateFormatter()));
+
+        var targetMetadata = RpcTargetMetadata.FromShape<IServer>();
+        serverRpc.AddLocalRpcTarget(targetMetadata, new Server(), null);
+
+        serverRpc.StartListening();
+        IServer proxy = clientRpc.Attach<IServer>();
+        clientRpc.StartListening();
+
+        int sum = await proxy.AddAsync(2, 5);
+        Console.WriteLine($"2 + 5 = {sum}");
+    }
+
+    private static IJsonRpcMessageFormatter CreateFormatter() => new NerdbankMessagePackFormatter()
+    {
+        TypeShapeProvider = Witness.GeneratedTypeShapeProvider,
+    };
+
+    [GenerateShapeFor<int>]
+    private partial class Witness;
+}
