7fe3b3613decd9bac157953333da727fff6cd1ca
Thanhy Mather <tmather@omnissa.com>
2026-02-01 16:27:36 -0800

    rename proxy

--- StreamJsonRpc.Aot.Client/Program.cs
+++ StreamJsonRpc.Aot.Client/Program.cs
@@ -19,28 +19,28 @@ static async Task RunAsync(NamedPipeClientStream pipe, Guid guid)
 {
     HeaderDelimitedMessageHandler messageHandler = new(pipe, SystemTextJson.CreateFormatter());
     JsonRpc jsonRpc = new(messageHandler);
-    IServer proxy = jsonRpc.Attach<IServer>();
+    IServer server = jsonRpc.Attach<IServer>();
     jsonRpc.StartListening();
 
     Console.WriteLine($"  ClientId: {guid}");
-    bool connected = await proxy.ConnectAsync(guid);
+    bool connected = await server.ConnectAsync(guid);
 
     if (connected)
     {
         int a = Random.Shared.Next(0, 10);
         int b = Random.Shared.Next(0, 10);
-        int sum = await proxy.AddAsync(a, b);
+        int sum = await server.AddAsync(a, b);
         Console.WriteLine($"  Calculating {a} + {b} = {sum}");
 
-        List<string> list = await proxy.GetListAsync();
+        List<string> list = await server.GetListAsync();
         Console.WriteLine($"  GetList:");
         Console.WriteLine(string.Join(Environment.NewLine, list.Select((v, i) => $"    [{i}] {v}")));
 
-        Dictionary<Guid, DateTime> dict = await proxy.GetDictionaryAsync();
+        Dictionary<Guid, DateTime> dict = await server.GetDictionaryAsync();
         Console.WriteLine($"  GetDictionary:");
         Console.WriteLine(string.Join(Environment.NewLine, dict.Select(kv => $"    {kv.Key}={kv.Value:O}")));
 
-        Dictionary<string, string> table = await proxy.GetTableAsync();
+        Dictionary<string, string> table = await server.GetTableAsync();
         Console.WriteLine($"  GetTable:");
         Console.WriteLine(string.Join(Environment.NewLine, table.Select(kv => $"    {kv.Key}={kv.Value:O}")));
     }
