d56f3a56ac54f22d008328e269cd2019caaa759d
Thanhy Mather <tmather@omnissa.com>
2026-02-03 10:41:02 -0800

    shared and concrete interface

--- StreamJsonRpc.Aot.Common/INumberStreamListener.cs
+++ StreamJsonRpc.Aot.Common/INumberStreamListener.cs
@@ -5,11 +5,16 @@ using StreamJsonRpc;
 
 namespace StreamJsonRpc.Aot.Common;
 
-// Shared code between client and server
-[JsonRpcContract, GenerateShape(IncludeMethods = MethodShapeFlags.PublicInstance)]
-public partial interface INumberStreamListener
+// Shared interface between client and server
+public partial interface IListener
 {
     Task OnNextValue(int value);
     Task OnError(string error);
     Task OnCompleted();
+}
+
+// Concreate interface
+[JsonRpcContract, GenerateShape(IncludeMethods = MethodShapeFlags.PublicInstance)]
+public partial interface INumberStreamListener : IListener
+{
 }
