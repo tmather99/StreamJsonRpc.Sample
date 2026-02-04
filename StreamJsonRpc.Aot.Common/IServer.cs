6d044bcfb60d4af4f3068f0093e07d31b96391bd
Thanhy Mather <tmather@omnissa.com>
2026-02-04 13:47:40 -0800

    comments IServer

--- StreamJsonRpc.Aot.Common/IServer.cs
+++ StreamJsonRpc.Aot.Common/IServer.cs
@@ -9,21 +9,25 @@ namespace StreamJsonRpc.Aot.Common;
 [JsonRpcContract, GenerateShape(IncludeMethods = MethodShapeFlags.PublicInstance)]
 public partial interface IServer
 {
+    // data type marshaling operations
     Task<bool> ConnectAsync(Guid guid);
     Task<int> AddAsync(int a, int b);
     Task<List<string>> GetListAsync();
     Task<Dictionary<Guid, DateTime>> GetDictionaryAsync();
     Task<Dictionary<string, string>> GetTableAsync();
 
+    // Counter observer operations
     Task SetObserver(IObserver<int> observer, CancellationToken cancellationToken);
     Task<IObserver<int>> GetObserver(CancellationToken cancellationToken);
 
     Task SetAsyncEnumerable(IAsyncEnumerable<int> values, CancellationToken cancellationToken);
     Task<IAsyncEnumerable<int>> GetAsyncEnumerable(CancellationToken cancellationToken);
 
+    // heatbeat start/stop operations
     Task SendTicksAsync(Guid guid);
     Task CancelTickOperation(Guid guid);
 
+    // mouse and number stream subscriptions
     Task SubscribeToNumberStream();
     Task SubscribeToMouseStream();
 }
