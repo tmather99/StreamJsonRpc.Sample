58c234acaed6c929c623cc7625f8f66af1a3cf2c
Thanhy Mather <tmather@omnissa.com>
2026-02-15 09:10:38 -0800

    LibraryImport

--- RegistryPidWatcherFull/src/NativeMethods.cs
+++ RegistryPidWatcherFull/src/NativeMethods.cs
@@ -1,27 +1,67 @@
-using System.Runtime.InteropServices;
-
 namespace RegistryPidWatcher;
 
-internal static class NativeMethods
+using System;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+
+[SupportedOSPlatform("windows")]
+// allows the generated interop layer to use pointers internally.
+internal static partial class NativeMethods
 {
     public const int KEY_NOTIFY = 0x0010;
     public const int REG_NOTIFY_CHANGE_NAME = 0x00000001;
     public const int REG_NOTIFY_CHANGE_LAST_SET = 0x00000004;
-    public static readonly UIntPtr HKEY_LOCAL_MACHINE = (UIntPtr)0x80000002u;
-    public static readonly UIntPtr HKEY_CURRENT_USER = (UIntPtr)0x80000001u;
 
-    [DllImport("advapi32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
-    public static extern int RegOpenKeyEx(UIntPtr hKey, string subKey, int ulOptions, int samDesired, out IntPtr phkResult);
+    public static readonly UIntPtr HKEY_LOCAL_MACHINE = 0x80000002u;
+    public static readonly UIntPtr HKEY_CURRENT_USER = 0x80000001u;
 
-    [DllImport("advapi32.dll", SetLastError = true)]
-    public static extern int RegNotifyChangeKeyValue(IntPtr hKey, bool bWatchSubtree, int dwNotifyFilter, IntPtr hEvent, bool fAsynchronous);
+    // -----------------------------
+    // Registry APIs
+    // -----------------------------
 
-    [DllImport("advapi32.dll")]
-    public static extern int RegCloseKey(IntPtr hKey);
+    [LibraryImport("advapi32.dll",
+        EntryPoint = "RegOpenKeyExW",
+        SetLastError = true,
+        StringMarshalling = StringMarshalling.Utf16)]
+    public static partial int RegOpenKeyEx(
+        UIntPtr hKey,
+        string subKey,
+        int ulOptions,
+        int samDesired,
+        out IntPtr phkResult);
 
-    [DllImport("kernel32.dll", SetLastError = true)]
-    public static extern IntPtr CreateEvent(IntPtr lpEventAttributes, bool bManualReset, bool bInitialState, string? lpName);
+    [LibraryImport("advapi32.dll",
+        EntryPoint = "RegNotifyChangeKeyValue",
+        SetLastError = true)]
+    public static partial int RegNotifyChangeKeyValue(
+        IntPtr hKey,
+        [MarshalAs(UnmanagedType.Bool)] bool bWatchSubtree,
+        int dwNotifyFilter,
+        IntPtr hEvent,
+        [MarshalAs(UnmanagedType.Bool)] bool fAsynchronous);
 
-    [DllImport("kernel32.dll")]
-    public static extern bool ResetEvent(IntPtr hEvent);
+    [LibraryImport("advapi32.dll",
+        EntryPoint = "RegCloseKey",
+        SetLastError = true)]
+    public static partial int RegCloseKey(IntPtr hKey);
+
+    // -----------------------------
+    // Event APIs
+    // -----------------------------
+
+    [LibraryImport("kernel32.dll",
+        EntryPoint = "CreateEventW",
+        SetLastError = true,
+        StringMarshalling = StringMarshalling.Utf16)]
+    public static partial IntPtr CreateEvent(
+        IntPtr lpEventAttributes,
+        [MarshalAs(UnmanagedType.Bool)] bool bManualReset,
+        [MarshalAs(UnmanagedType.Bool)] bool bInitialState,
+        string? lpName);
+
+    [LibraryImport("kernel32.dll",
+        EntryPoint = "ResetEvent",
+        SetLastError = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static partial bool ResetEvent(IntPtr hEvent);
 }
